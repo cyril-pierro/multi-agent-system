140af20027aedf6c7224b57cfe09747d13811cc4
cyril-pierro <fiopapa32@gmail.com>
2025-10-01 16:59:28 +0000

    add various constants

---
+++ constants/__init__.py
@@ -0,0 +1,5 @@
+from config.setting import settings
+
+
+MODEL_NAME = "gemini-2.5-flash-preview-05-20"
+API_URL = f"https://generativelanguage.googleapis.com/v1beta/models/{MODEL_NAME}:generateContent?key={settings.GEMINI_API_KEY}"
